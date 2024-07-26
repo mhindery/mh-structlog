8fe63bc1c0f8bbb5a6088ca6f4db8be951500c0a
Mathieu Hinderyckx <mathieu.hinderyckx@gmail.com>
2024-07-26 13:42:22 +0200

    Named logger improvement

--- src/mh_structlog/__init__.py
+++ src/mh_structlog/__init__.py
@@ -5,16 +5,19 @@ import structlog
 
 from .config import filter_named_logger, setup
 from .django import StructLogAccessLoggingMiddleware
+from .utils import determine_name_for_logger
 
 
 def getLogger(name: t.Optional[str] = None):  # noqa: ANN201, N802
     """Return a named logger."""
+    if name is None:
+        name = determine_name_for_logger()
     return structlog.get_logger(name)
 
 
 def get_logger(name: t.Optional[str] = None):  # noqa: ANN201
     """Return a named logger."""
-    return structlog.get_logger(name)
+    return getLogger(name)
 
 
 __all__ = [
