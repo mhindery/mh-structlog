79555e2751346ccb04de46dcdcea8ac63c16ae87
Mathieu Hinderyckx <mathieu.hinderyckx@gmail.com>
2025-12-11 23:35:43 +0100

    freeze

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -5,6 +5,7 @@ from typing import TextIO
 
 import orjson
 import pytest
+from freezegun import freeze_time
 from structlog import reset_defaults
 
 from mh_structlog import ERROR, filter_named_logger, get_logger, setup
@@ -21,6 +22,7 @@ def capture_output() -> Generator[tuple[TextIO, TextIO]]:
         yield out, err
 
 
+@freeze_time("2025-12-11 12:01:02")
 def test_setup_twice():
     reset_defaults()
 
@@ -30,12 +32,11 @@ def test_setup_twice():
 
     data = orjson.loads(out.getvalue())
 
-    data.pop("timestamp")
-
     assert data == {
         'level': 'warning',
         'logger': 'mh_structlog',
         'message': 'logging was already configured, so I return and do nothing.',
+        'timestamp': '2025-12-11T12:01:02Z',
     }
 
 
@@ -49,6 +50,7 @@ def test_setup_invalid_params():
         setup(log_format='invalid_format')
 
 
+@freeze_time("2025-12-11 12:01:02")
 def test_logging_json():
     reset_defaults()
 
@@ -61,13 +63,11 @@ def test_logging_json():
 
     assert isinstance(data, dict)
 
-    assert "timestamp" in data
-    data.pop("timestamp")
-
     assert data == {
         'keyA': 'valueA',
         'keyB': 100,
         'logger': 'test_logger_json',
         'level': 'info',
         'message': 'JSON log message',
+        'timestamp': '2025-12-11T12:01:02Z',
     }
