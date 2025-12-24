509953e03d5290883d38ad746da69275fec8d77a
Mathieu Hinderyckx <mathieu.hinderyckx@gmail.com>
2025-12-24 14:51:43 +0100

    unittests

--- src/mh_structlog/aws.py
+++ src/mh_structlog/aws.py
@@ -7,6 +7,12 @@ from aws_lambda_powertools.utilities.typing import LambdaContext
 is_cold_start = True
 
 
+def _reset_cold_start_flag() -> None:
+    """Reset the cold start flag to True. This is primarily intended for testing purposes."""
+    global is_cold_start  # noqa: PLW0603
+    is_cold_start = True
+
+
 def bind_lambda_context(lambda_context: LambdaContext) -> None:
     """Bind AWS Lambda context information to the structlog context variables, so log entries contain Lambda function metadata.
 
@@ -14,6 +20,8 @@ def bind_lambda_context(lambda_context: LambdaContext) -> None:
         lambda_context (LambdaContext): The AWS Lambda context object.
     """
     global is_cold_start  # noqa: PLW0603
+
+    print(is_cold_start)
 
     if lambda_context:
         structlog.contextvars.clear_contextvars()
@@ -30,5 +38,4 @@ def bind_lambda_context(lambda_context: LambdaContext) -> None:
         )
 
         # After the first invocation of an environment, set cold_start to False for further invocations
-        if is_cold_start:
-            is_cold_start = False
+        is_cold_start = False
