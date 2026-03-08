edce7cc87e16a95065ee12935ff7f7479ee897b2
Mathieu Hinderyckx <mathieu.hinderyckx@gmail.com>
2026-03-08 21:18:15 +0100

    asdf

--- tests/root_urlconf.py
+++ tests/root_urlconf.py
@@ -6,12 +6,4 @@ def my_view(request):
     return HttpResponse("Hello, world!")
 
 
-def redirect(request):
-    return HttpResponseRedirect("/redirect_to/")
-
-
-def http_404_view(request):
-    return HttpResponse("Not found", status=404)
-
-
 urlpatterns = [path("", my_view, name="main-view")]
